011efc4b5efe022da47371e5f4fe8f6dbabe8c19
serdarferhat <cylnsrdrr@gmail.com>
2023-03-12 22:43:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles/styles.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        merhaba
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/ListPosts/ListPostsView.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+
+const ListPostsView = () => {
+                useEffect(() => {
+
+
+                                return () => {
+                                                
+                                }
+                }, [])
+
+                return (
+                                <div className='container'>
+
+                                </div>
+                )
+}
+
+export default ListPostsView
