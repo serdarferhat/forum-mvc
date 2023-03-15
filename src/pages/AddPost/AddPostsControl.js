de55e2f9e0f3f7887657e8d6ed757e928bee30a9
serdarferhat <cylnsrdrr@gmail.com>
2023-03-15 10:46:19 +0300

    model_veri-control_fonk-view_görüntü

--- src/pages/AddPost/AddPostsControl.js
+++ src/pages/AddPost/AddPostsControl.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import AddPostsModel from './AddPostsModel'
 import AddPostsView from './AddPostsView'
 
 const AddPostsControl = () => {
+  const navigate=useNavigate()
+  const model=new AddPostsModel()
+  // console.log(model)
+  const [form,setForm]=useState(model.state)
+
+  
+
   //input değerleri
   const onInputChange=(label,value)=>{
-    console.log(label,value)
+    // console.log(label,value)
+    var newInput={...form};newInput[label]=value;
+    // console.log(newInput)
+    setForm(newInput)
   }
   //button onsubmit
-  const handleSubmit=()=>{
-
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    // console.log(form)
+    if(!form.user||!form.title||!form.text){
+      alert("Verileri doldurun")
+    }else{
+      axios.post("http://localhost:3035/posts",form)
+      .then(()=>navigate("/"))
+    }
   }
   return <AddPostsView 
   onInputChange={onInputChange}
---
+++ src/pages/AddPost/AddPostsModel.js
@@ -0,0 +1,10 @@
+class AddPostsModel{
+  state={
+         id:new Date().getTime(),
+         user:"",
+         title:"",
+         text:""   
+};
+}
+
+export default AddPostsModel
